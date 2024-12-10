05978782067291ba1fed61d979efd1a99bc1f60a
alexChiang <alexchiang.cg@gmail.com>
2024-12-10 16:57:16 +0800

    feat: profile components

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,64 +3,65 @@ import IGContainer from "components/IGContainer";
 import IGStory from "./components/IGStory";
 import IGPost from "./components/IGPost";
 import IGUser from "components/IGUser";
+import IGProfile from "./components/IGProfile";
 
 const IGPostList: React.FC = () => {
   const data = [
     {
-      "id": 1,
-      "location": "全家",
-      "account": "alex1234",
-      "avatar": "/images/avatars/a1.png",
-      "photo": "/images/posts/main1.png",
-      "likes": 999,
-      "description": "我的老天鵝！",
-      "hashTags": "#鵝鵝",
-      "createTime": "1 HOURS AGO"
+      id: 1,
+      location: "全家",
+      account: "alex1234",
+      avatar: "/images/avatars/a1.png",
+      photo: "/images/posts/main1.png",
+      likes: 999,
+      description: "我的老天鵝1",
+      hashTags: "#鵝鵝",
+      createTime: "1 HOURS AGO",
     },
     {
-      "id": 2,
-      "location": "7-11你好門市",
-      "account": "__0831_xxx__",
-      "avatar": "/images/avatars/a2.png",
-      "photo": "/images/posts/main2.png",
-      "likes": 333,
-      "description": "我的老天鵝！",
-      "hashTags": "#鵝鵝",
-      "createTime": "5 HOURS AGO"
+      id: 2,
+      location: "7-11你好門市",
+      account: "__0831_xxx__",
+      avatar: "/images/avatars/a2.png",
+      photo: "/images/posts/main2.png",
+      likes: 333,
+      description: "我的老天鵝2",
+      hashTags: "#鵝鵝",
+      createTime: "5 HOURS AGO",
     },
     {
-      "id": 3,
-      "location": "Taipei",
-      "account": "gogogo_0214",
-      "avatar": "/images/avatars/a3.png",
-      "photo": "/images/posts/main3.png",
-      "likes": 777,
-      "description": "我的老天鵝！",
-      "hashTags": "#鵝鵝",
-      "createTime": "6 HOURS AGO"
+      id: 3,
+      location: "Taipei",
+      account: "gogogo_0214",
+      avatar: "/images/avatars/a3.png",
+      photo: "/images/posts/main3.png",
+      likes: 777,
+      description: "我的老天鵝3",
+      hashTags: "#鵝鵝",
+      createTime: "6 HOURS AGO",
     },
     {
-      "id": 4,
-      "location": "信義區",
-      "account": "zoe_0000",
-      "avatar": "/images/avatars/a4.png",
-      "photo": "/images/posts/main4.png",
-      "likes": 824,
-      "description": "我的老天鵝！",
-      "hashTags": "#鵝鵝",
-      "createTime": "7 HOURS AGO"
+      id: 4,
+      location: "信義區",
+      account: "zoe_0000",
+      avatar: "/images/avatars/a4.png",
+      photo: "/images/posts/main4.png",
+      likes: 824,
+      description: "我的老天鵝4",
+      hashTags: "#鵝鵝",
+      createTime: "7 HOURS AGO",
     },
     {
-      "id": 5,
-      "location": "南港區",
-      "account": "zoe_0000",
-      "avatar": "/images/avatars/a5.png",
-      "photo": "/images/posts/main5.png",
-      "likes": 98,
-      "description": "我的老天鵝！",
-      "hashTags": "#鵝鵝",
-      "createTime": "10 HOURS AGO"
-    }
+      id: 5,
+      location: "南港區",
+      account: "zoe_0000",
+      avatar: "/images/avatars/a5.png",
+      photo: "/images/posts/main5.png",
+      likes: 98,
+      description: "我的老天鵝5",
+      hashTags: "#鵝鵝",
+      createTime: "10 HOURS AGO",
+    },
   ];
   return (
     <>
@@ -107,11 +108,7 @@ const Home: React.FC = () => {
           </div>
           {/* 右邊 */}
           <div className="hidden lg:block lg:w-[424px]">
-            <IGUser
-              account="alex"
-              location="taiwan"
-              avatar="/images/avatars/a1.png"
-            />
+            <IGProfile />
           </div>
         </div>
       </IGContainer>
