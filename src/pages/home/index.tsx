82b7ac53a6c93e9bf86e93a065e3d2ddb59a6504
alexChiang <alexchiang.cg@gmail.com>
2024-12-10 15:58:42 +0800

    feat: 基本好友 comp

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,29 @@
 import IGHeader from "components/IGHeader";
+import IGContainer from "components/IGContainer";
+import IGStory from "./components/IGStory";
+
+import IGUser from "components/IGUser";
 
 const Home: React.FC = () => {
   return (
     <>
       <IGHeader />
-      <h1>Home content</h1>
+      <IGContainer>
+        <div className="flex lg:justify-center">
+          {/* 左邊 */}
+          <div className="w-full lg:w-[600px]">
+            <IGStory />
+          </div>
+          {/* 右邊 */}
+          <div className="hidden lg:block lg:w-[424px]">
+            <IGUser
+              account="alex"
+              location="taiwan"
+              avatar="/images/avatars/a1.png"
+            />
+          </div>
+        </div>
+      </IGContainer>
     </>
   );
 };
