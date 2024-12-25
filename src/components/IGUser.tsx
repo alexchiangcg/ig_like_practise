a66fd942bed9fce966dac69c268059db67fa88fa
alexChiang <alexchiang.cg@gmail.com>
2024-12-25 17:10:25 +0800

    perf: 使用 memo

--- src/components/IGUser.tsx
+++ src/components/IGUser.tsx
@@ -4,6 +4,8 @@ import { useAppDispatch } from "../hooks";
 // action
 import { follow, unFollow } from "../slices/friendSlice";
 
+import { memo } from "react";
+
 type IGUserProps = {
   size?: "medium" | "small";
   showFollow?: boolean;
@@ -14,53 +16,55 @@ type IGUserProps = {
   id?: number;
 };
 
-const IGUser: React.FC<IGUserProps> = ({
-  size = "small",
-  showFollow = false,
-  isFollowing = false,
-  account,
-  location,
-  avatar,
-  id,
-}) => {
-  const dispatch = useAppDispatch();
+const IGUser: React.FC<IGUserProps> = memo(
+  ({
+    size = "small",
+    showFollow = false,
+    isFollowing = false,
+    account,
+    location,
+    avatar,
+    id,
+  }) => {
+    const dispatch = useAppDispatch();
 
-  function followClickHandler() {
-    if (id === undefined) return;
-    if (isFollowing) {
-      dispatch(unFollow(id));
-    } else {
-      dispatch(follow(id));
+    function followClickHandler() {
+      if (id === undefined) return;
+      if (isFollowing) {
+        dispatch(unFollow(id));
+      } else {
+        dispatch(follow(id));
+      }
     }
-  }
-  return (
-    <div className="flex h-[70px] items-center box-border px-4">
-      <div
-        className={`${
-          size === "small" ? "w-[40px] h-[40px]" : "w-[60px] h-[60px]"
-        } overflow-hidden rounded-full`}
-        style={{
-          backgroundImage: `url(${avatar})`,
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-        }}
-      ></div>
-      <div className="ml-4">
-        <p className="font-bold text-sm">{account}</p>
-        <p className="text-gray-400 text-xs">{location}</p>
-      </div>
-      {showFollow && (
-        <button
+    return (
+      <div className="flex h-[70px] items-center box-border px-4">
+        <div
           className={`${
-            isFollowing ? "text-gray-700" : "text-blue-400"
-          } ml-auto text-xs font-bold cursor-pointer bg-transparent border-none`}
-          onClick={followClickHandler}
-        >
-          {isFollowing ? "FOLLOWING" : "FOLLOW"}
-        </button>
-      )}
-    </div>
-  );
-};
+            size === "small" ? "w-[40px] h-[40px]" : "w-[60px] h-[60px]"
+          } overflow-hidden rounded-full`}
+          style={{
+            backgroundImage: `url(${avatar})`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div className="ml-4">
+          <p className="font-bold text-sm">{account}</p>
+          <p className="text-gray-400 text-xs">{location}</p>
+        </div>
+        {showFollow && (
+          <button
+            className={`${
+              isFollowing ? "text-gray-700" : "text-blue-400"
+            } ml-auto text-xs font-bold cursor-pointer bg-transparent border-none`}
+            onClick={followClickHandler}
+          >
+            {isFollowing ? "FOLLOWING" : "FOLLOW"}
+          </button>
+        )}
+      </div>
+    );
+  }
+);
 
 export default IGUser;
